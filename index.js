f73ff2144429e6f2516541acd0237d8dd0c129c8
beargod <q549917812>
2018-06-09 22:55:15 +0800

    first

---
+++ index.js
@@ -0,0 +1,402 @@
+$(document).ready(function () {
+    $('.slider-input').jRange({
+        from: 1,
+        to: 100,
+        step: 1,
+        scale: [1, 25, 50, 75, 100],
+        format: '%s',
+        width: 200,
+        showLabels: true,
+        isRange: true,
+        theme: "theme-blue",
+    });
+});
+var NebPay = require("nebpay"); //https://github.com/nebulasio/nebPay
+var vm = new Vue({
+    el: ".vue-app",
+    data: {
+        address: "",
+        lowest: 0,
+        commission: 0,
+        jackpot: 0,
+        all_history: [],
+        user_history: [],
+        history_size: 0,
+        bet_up: 1,
+        bet_down: 51,
+        bet_size: 0.0001,
+        bet_profit: 0.0,
+        web_wallet: 1,
+        wallet_balance: 0,
+        chance: 50,
+        short_hash: "",
+        tx_hash: "aaaaaa",
+        my_profit: 0,
+        my_balance: 0,
+        last_roll: 0,
+        last_award: 0,
+        take_balance: 0
+    },
+    methods: {
+        min: function () {
+            vm.$data.bet_size = vm.$data.lowest;
+        },
+        random: function () {
+            if (vm.$data.wallet_balance < vm.$data.lowest) {
+                vm.$data.bet_size = 0;
+            } else {
+                vm.$data.bet_size = RandomNumBoth(vm.$data.lowest, vm.$data.wallet_balance);
+            }
+        },
+        max: function () {
+            vm.$data.bet_size = vm.$data.wallet_balance;
+        },
+        roll: function () {
+            roll();
+        },
+        copy_hash: function () {
+            var obj = document.getElementById("hide_txhash");
+            obj.select();
+            document.execCommand("Copy");
+            alert("复制链接成功！");
+        },
+        take_out: function () {
+            takeOut();
+        }
+    },
+    watch: {
+        bet_size: function (newVal, oldVal) {
+
+            var newVal = Math.round(newVal * 10000) / 10000;
+            this.bet_size = newVal;
+            if (newVal < vm.$data.wallet_balance && newVal > vm.$data.lowest) {
+                resizeProfit();
+                return;
+            }
+
+            if (vm.$data.wallet_balance < vm.$data.lowest) {
+                this.bet_size = 0;
+                resizeProfit();
+                return;
+            }
+
+            if (vm.$data.wallet_balance < newVal) {
+                this.bet_size = vm.$data.wallet_balance;
+                resizeProfit();
+                return;
+            }
+            if (vm.$data.lowest > newVal) {
+                this.bet_size = vm.$data.lowest;
+                resizeProfit();
+            }
+        },
+
+        take_balance: function (newVal, oldVal) {
+
+            var newVal = Math.round(newVal * 10000) / 10000;
+            if (newVal < 0) {
+                newVal = 0;
+            }
+            if (newVal > this.my_balance) {
+                newVal = this.my_balance;
+            }
+            this.take_balance = newVal;
+        },
+        bet_down: function (newVal, oldVal) {
+            var newVal = Math.round(newVal);
+            if (newVal > 100) {
+                newVal = 100;
+            }
+            if (newVal - vm.$data.bet_up > 100 - vm.$data.commission) {
+                newVal = 100 - vm.$data.commission + vm.$data.bet_up;
+            }
+            if (newVal < vm.$data.bet_up) {
+                newVal = vm.$data.bet_up;
+            }
+
+            this.bet_down = newVal;
+            vm.$data.chance = vm.$data.bet_down - vm.$data.bet_up;
+            $('.slider-input').jRange('setValue', jrVal());
+            resizeProfit();
+        },
+        bet_up: function (newVal, oldVal) {
+            var newVal = Math.round(newVal);
+            if (newVal < 1) {
+                newVal = 1;
+            }
+            if (newVal > vm.$data.bet_down) {
+                newVal = vm.$data.bet_down;
+            }
+            if (vm.$data.bet_down - newVal > 100 - vm.$data.commission) {
+                newVal = vm.$data.commission + vm.$data.bet_up;
+            }
+
+            this.bet_up = newVal;
+            vm.$data.chance = vm.$data.bet_down - vm.$data.bet_up;
+            $('.slider-input').jRange('setValue', jrVal());
+            resizeProfit();
+        }
+    }
+});
+
+$("#hide_txhash").hide();
+
+var nebPay = new NebPay();
+var dappAddress = "n1fPRVXkw7KczBVSCk5x3yr7x68GFRJqSpy";
+
+//检查扩展是否已安装
+//如果安装了扩展，var“webExtensionWallet”将被注入到web页面中1
+if (typeof(webExtensionWallet) === "undefined") {
+    //alert ("扩展钱包未安装，请先安装.")
+    vm.$data.web_wallet = 0;
+}
+//获取钱包地址
+var nebulas = require("nebulas"), Account = nebulas.Account, neb = new nebulas.Neb();
+neb.setRequest(new nebulas.HttpRequest("https://mainnet.nebulas.io"));
+getWalletInfo((address) => {
+    vm.$data.address = address;
+    //获取钱包余额
+    getBalance();
+});
+//获取最小值
+getLowest();
+//获取奖池
+getJackpot();
+
+getUser();
+
+var intervalQuery;
+
+
+function takeOut() {
+    var value = vm.bet_size;
+    var callArgs = "[" + numberToBigNumber(vm.take_balance) + "]"
+    nebPay.call(dappAddress, value, "takeout", callArgs, {
+        listener: cbTakeout
+    });
+}
+
+function cbTakeout(rs) {
+    cbnetwork(rs);
+    if (rs.hasOwnProperty('txhash') && rs.txhash) {
+        alert("数据以及打包提交到链上，等待确认后将会自动刷新数据");
+        intervalQuery = setInterval(function () {
+            neb.api.getTransactionReceipt({hash: vm.tx_hash}).then(function (resp) {
+                console.log("tx result: " + JSON.stringify(resp))
+                if (resp.status == 1) {
+                    clearInterval(intervalQuery);
+                    alert("取出余额成功");
+                    getJackpot();
+                    getUser();
+                }
+            }).catch(function (err) {
+                console.log(err);
+            });
+        }, 5000);
+    }
+
+}
+
+function roll() {
+    if (intervalQuery) {
+        alert("请等待上个交易完成");
+        return;
+    }
+    var value = vm.bet_size;
+    var callArgs = "[" + vm.bet_up + "," + vm.bet_down + "]"
+    nebPay.call(dappAddress, value, "play", callArgs, {
+        listener: cbRoll
+    });
+}
+
+function cbRoll(rs) {
+    cbnetwork(rs);
+    if (rs.hasOwnProperty('txhash') && rs.txhash) {
+        vm.tx_hash = rs.txhash;
+        var length = vm.tx_hash.length;
+        vm.short_hash = vm.tx_hash.substr(0, 5) + "..." + vm.tx_hash.substr(length - 6, length - 1);
+        alert("数据以及打包提交到链上，等待确认后将会自动刷新数据");
+        intervalQuery = setInterval(function () {
+            neb.api.getTransactionReceipt({hash: vm.tx_hash}).then(function (resp) {
+                console.log("tx result: " + JSON.stringify(resp))
+                if (resp.status == 1) {
+                    clearInterval(intervalQuery);
+                    successCb(resp);
+                }
+            }).catch(function (err) {
+                console.log(err);
+            });
+        }, 5000);
+    }
+
+}
+
+
+function successCb(rs) {
+    var newRoll = JSON.parse(rs.result);
+    if (!newRoll) {
+        return;
+    }
+    vm.last_roll = newRoll.result;
+    if (new BigNumber(newRoll.award).gt(0)) {
+        alert("恭喜你，赢得了" + bigNumberToNumber(newRoll.award) + "nas");
+    }
+    vm.last_award = bigNumberToNumber(newRoll.award);
+    getJackpot();
+    getUser();
+}
+
+function getUser() {
+    var value = 0;
+    var callArgs = ""
+    nebPay.simulateCall(dappAddress, value, "getUser", callArgs, {
+        listener: cbUser
+    });
+}
+
+function cbUser(rs) {
+    cbnetwork(rs);
+    var result = JSON.parse(rs.result);
+    if (!result) {
+        return;
+    }
+
+    vm.my_balance = bigNumberToNumber(toInt(result.balance));
+    vm.my_profit = bigNumberToNumber(toInt(result.award));
+    vm.user_history = result.history;
+}
+
+function getJackpot() {
+    var value = 0;
+    var callArgs = ""
+    nebPay.simulateCall(dappAddress, value, "getJackpot", callArgs, {
+        listener: cbJackpot
+    });
+}
+
+function cbJackpot(rs) {
+    cbnetwork(rs);
+    vm.jackpot = bigNumberToNumber(toInt(rs.result));
+}
+
+function getCommission() {
+    var value = 0;
+    var callArgs = ""
+    nebPay.simulateCall(dappAddress, value, "getCommission", callArgs, {
+        listener: cbCommission
+    });
+}
+
+function resizeProfit() {
+    vm.$data.bet_profit = (vm.$data.bet_size) * (1 / vm.$data.chance * 100 - 1 - vm.$data.commission);
+    vm.$data.bet_profit = Math.round(vm.$data.bet_profit * 10000) / 10000;
+}
+
+function jrVal() {
+    return vm.$data.bet_up + "," + vm.$data.bet_down;
+}
+
+function cbCommission(rs) {
+    cbnetwork(rs);
+    vm.$data.commission = Math.round(rs.result * 100);
+    if (vm.$data.bet_down - vm.$data.bet_up > 100 - vm.$data.commission) {
+        vm.$data.bet_down = 100 - vm.$data.commission + vm.$data.bet_up;
+    }
+}
+
+
+function getLowest() {
+    var value = 0;
+    var callArgs = ""
+    nebPay.simulateCall(dappAddress, value, "getLowest", callArgs, {
+        listener: cbLowest
+    });
+}
+
+function cbLowest(rs) {
+    cbnetwork(rs);
+    var result = rs.result;
+    vm.$data.lowest = bigNumberToNumber(toInt(result));
+}
+
+function cbnetwork(rs) {
+    if (rs == "Network Error") {
+        alert("星云链主网抽风啦，请稍后再试")
+        return
+    }
+    if (rs == "Error: Transaction rejected by user") {
+        alert("由于你拒绝了交易，数据没有成功上传到链上");
+        return
+    }
+    if (!rs.execute_err && rs.execute_err != "") {
+        alert(rs.execute_err);
+    }
+}
+
+function bigNumberToNumber(num) {
+    var number = new BigNumber(num).toNumber();
+    number = Math.round(number / Math.pow(10, 14)) / 10000;
+    ;
+    return number;
+}
+
+function numberToBigNumber(num) {
+    var number = new BigNumber(num);
+    return number.mul(Math.pow(10, 18));
+}
+
+function toInt(str) {
+    return str.replace(new RegExp("\"", "gm"), "");
+}
+
+function getWalletInfo(callback) {
+    window.postMessage({
+        "target": "contentscript",
+        "data": {},
+        "method": "getAccount",
+    }, "*");
+
+    window.addEventListener('message', function (e) {
+        if (e.data && e.data.data) {
+            if (e.data.data.account) { //这就是当前钱包中的地址
+                var address = e.data.data.account;
+                if (address && address.length != 0) {
+                    if (callback) {
+                        callback(address);
+                    }
+                }
+            }
+        }
+    });
+}
+
+function getBalance() {
+    neb.api.getAccountState(vm.$data.address).then(function (state) {
+        state = state.result || state;
+        vm.$data.wallet_balance = bigNumberToNumber(state.balance);
+    }).catch(function (err) {
+        console.log("err:", err);
+    });
+}
+
+//获得随机数
+function RandomNumBoth(Min, Max) {
+    var Range = Max - Min;
+    var Rand = Math.random();
+    var num = Min + Math.round(Rand * Range); //四舍五入
+    $('.bet_number').val(num);
+    return num;
+};
+
+$(document).ready(function () {
+    // 获得区间上下限,split("")是把字符串分割成字符串数组
+    $('.slider-input').change(function () {
+        var change = $('.slider-input').val();
+
+        var nn = change.split(',');
+        vm.$data.bet_up = parseFloat(nn[0]);
+        vm.$data.bet_down = parseFloat(nn[1]);
+        vm.$data.chance = vm.$data.bet_down - vm.$data.bet_up;
+    });
+});
+
---
+++ nasroll.js
@@ -0,0 +1,318 @@
+"use strict";
+
+
+var Roll = function (text) {
+
+    if (text) {
+
+        var obj = JSON.parse(text);
+        this.id = obj.id;
+        this.nas = obj.nas;
+        this.under = obj.under;
+        this.award = obj.award;
+        this.result = obj.result;
+        this.begin = obj.begin;
+        this.from = obj.from;
+    } else {
+        this.id = 0;
+        this.nas = new BigNumber(0);
+        this.under = 0;
+        this.award = new BigNumber(0);
+        this.result = 0;
+        this.begin = 0;
+        this.from = "";
+    }
+
+};
+
+Roll.prototype = {
+    toString: function () {
+        return JSON.stringify(this);
+    }
+
+};
+
+var User = function (text) {
+
+    if (text) {
+        var obj = JSON.parse(text);
+        this.history = obj.history;
+        this.balance = obj.balance;
+        this.use = obj.use;
+        this.award = obj.award;
+    } else {
+        this.history = [];
+        this.balance =  new BigNumber(0);
+        this.use =  new BigNumber(0);
+        this.award =  new BigNumber(0);
+    }
+
+};
+
+User.prototype = {
+    toString: function () {
+        return JSON.stringify(this);
+    }
+
+};
+
+
+var NasRoll = function () {
+
+    LocalContractStorage.defineMapProperty(this, "roll", {
+
+        parse: function (text) {
+
+            return new Roll(text);
+
+        },
+
+        stringify: function (o) {
+
+            return o.toString();
+
+        }
+
+    });
+
+    LocalContractStorage.defineMapProperty(this, "user", {
+
+        parse: function (text) {
+
+            return new User(text);
+
+        },
+
+        stringify: function (o) {
+
+            return o.toString();
+
+        }
+
+    });
+
+    LocalContractStorage.defineProperty(this, "jackpot");
+
+    LocalContractStorage.defineProperty(this, "owner");
+
+    LocalContractStorage.defineProperty(this, "rollSize");
+
+    LocalContractStorage.defineProperty(this, "commission");
+
+    LocalContractStorage.defineProperty(this, "myCommission");
+
+
+
+    LocalContractStorage.defineProperty(this, "lowest");
+
+};
+
+
+NasRoll.prototype = {
+
+    init: function () {
+        this.jackpot = new BigNumber(0);
+        this.lowest = new BigNumber(10000000000000);
+        this.rollSize = 0;
+        this.awardSize = 0;
+        this.commission = 0.0;
+        this.myCommission = new BigNumber(0);
+        this.owner = Blockchain.transaction.from;
+    },
+
+    // roll
+    play: function (begin,under) {
+        if(!under||!begin||under<1||under>100||under<begin||begin<1||begin-under>(99-(this.commission*100))){
+            throw new Error("number error");
+        }
+        var value =Blockchain.transaction.value;
+        if(value.lt(this.lowest)){
+            throw new Error("value error");
+        }
+        var from = Blockchain.transaction.from;
+        var random = this._getRandom(1,100);
+        var award = (value.div((under-begin+1.0)/100.0)).round();
+        var commissionAward = (award.mul(this.commission)).round();
+        award = award.sub(commissionAward);
+        var win = random<=under&&random>=begin;
+        var roll = new Roll();
+
+        roll.id = this.rollSize;
+        roll.nas = value;
+        roll.under = under;
+
+        roll.result = random;
+        roll.begin = begin;
+        roll.from = from;
+        var user = this.user.get(from);
+        if(!user){
+            user = new User();
+        }
+        user.history.push(roll.id);
+        user.use = new BigNumber(user.use).plus(value);
+        if(win){
+            this.myCommission = commissionAward.add(this.myCommission);
+            var awardHistory = LocalContractStorage.get("awardHistory");
+            if(!awardHistory){
+                awardHistory = new Array();
+            }
+            awardHistory.push(roll.id);
+            LocalContractStorage.set("awardHistory",awardHistory);
+            roll.award = award;
+            user.award = new BigNumber(user.award).plus(award);
+            if(award>this.jackpot){
+                user.balance = new BigNumber(user.balance).plus(award);
+            }else {
+                this._transfer(from,award);
+                this.jackpot = new BigNumber(this.jackpot).sub(award);
+            }
+        }else {
+            roll.award = 0;
+        }
+        this.jackpot = new BigNumber(this.jackpot).plus(value);
+        this.user.set(from,user);
+        this.roll.set(roll.id,roll);
+        this.rollSize++;
+        return roll;
+    },
+
+    _transfer: function(from,amount){
+        var result = Blockchain.transfer(from, amount);
+        if (!result) {
+            throw new Error("transfer failed.");
+        }
+        Event.Trigger("BankVault", {
+            Transfer: {
+                from: Blockchain.transaction.to,
+                to: from,
+                value: amount.toString()
+            }
+        });
+    },
+
+    _getRandom: function (min, max) {
+        var r = Math.random() * (max - min);
+        var re = Math.round(r + min);
+        re = Math.max(Math.min(re, max), min)
+        return re;
+    },
+
+    // 取钱
+    takeout: function (value) {
+        var from = Blockchain.transaction.from;
+        var user = this.user.get(from);
+        if(value<=0){
+            throw new Error("value error");
+        }
+        var amount = new BigNumber(value).round();
+        if(amount.gt(user.balance)){
+            throw new Error("用户余额不够");
+        }
+        if(amount.gt(this.jackpot)){
+            throw new Error("奖池余额不够");
+        }
+        this._transfer(from,value);
+
+        user.balance=new BigNumber(user.balance).sub(value);
+        this.user.set(from,user);
+
+        this.jackpot = new BigNumber(this.jackpot).sub(value);
+    },
+
+    // 用户roll历史
+    userHistory: function () {
+        var from = Blockchain.transaction.from;
+        var user = this.user.get(from);
+        if(!user){
+            throw new Error("该用户没有roll历史");
+        }
+        var history = [];
+        for (var i = 0; i < user.history.length; i++) {
+            history.push(this.roll.get(user.history[i]));
+        }
+        return history;
+    },
+
+    // 奖励历史大小
+    getSize: function(){
+        var awardHistory = LocalContractStorage.get("awardHistory");
+        return awardHistory?awardHistory.length:0;
+    },
+
+    // 奖励历史数据
+    getHistory: function (begin,end) {
+        if(begin>end||begin<0||begin>this.getSize()){
+            throw new Error("size error");
+        }
+        end = end>this.getSize()?this.getSize():end;
+        var history = [];
+        var awardHistory = LocalContractStorage.get("awardHistory");
+        for (var i = begin; i < end; i++) {
+            history.push(this.roll.get(awardHistory[i]));
+        }
+        return history;
+    },
+
+    // 获取用户信息
+    getUser: function () {
+        var from = Blockchain.transaction.from;
+        return this.user.get(from);
+    },
+
+    // 奖池
+    getJackpot: function () {
+        return this.jackpot;
+    },
+
+    // 获取佣金百分比
+    getCommission: function () {
+        return this.commission;
+    },
+
+    setCommission:function (commission) {
+        this._onlyOwner();
+        this.commission = commission;
+    },
+
+    getMyCommission: function(){
+        this._onlyOwner();
+        return this.myCommission;
+    },
+
+    getLowest: function(){
+        return this.lowest;
+    },
+
+    setLowest: function(value){
+        this._onlyOwner();
+        return this.lowest = new BigNumber(value);
+    },
+
+    takeoutCommission: function(value){
+        this._onlyOwner();
+        var from = Blockchain.transaction.from;
+        if(value<=0){
+            throw new Error("value error");
+        }
+        var amount = new BigNumber(value).round();
+        if(amount.gt(this.myCommission)){
+            throw new Error("用户余额不够");
+        }
+        if(amount.gt(this.jackpot)){
+            throw new Error("奖池余额不够");
+        }
+        this._transfer(from,value);
+        this.jackpot=new BigNumber(this.jackpot).sub(value);
+        this.myCommission=new BigNumber(this.myCommission).sub(value);
+    },
+
+    _onlyOwner: function () {
+        var from = Blockchain.transaction.from;
+        if(from!=this.owner){
+            throw new Error("not owner");
+        }
+    }
+};
+
+
+module.exports = NasRoll;
